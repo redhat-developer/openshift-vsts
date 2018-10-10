92863e26caad63843f143ce478886d81fe07b169
Hardy Ferentschik <hardy@ferentschik.de>
2018-10-10 16:43:18 +0200

    Issue #12 Ensuring AGENT_OS=Linux for oc-cmd task
    - Adding unit tests using VSTS mocks

--- tasks/oc-cmd/src/task.ts
+++ tasks/oc-cmd/src/task.ts
@@ -4,23 +4,29 @@ import tl = require('vsts-task-lib/task');
 import oc = require('./oc-cmd')
 import install = require('./oc-install')
 
-let version = tl.getInput('version');
-let endpoint = tl.getInput('k8sService');
-let kubeconfig = tl.getEndpointAuthorizationParameter(endpoint, 'kubeconfig', true);
-let argLine = tl.getInput('cmd');
+if (process.env['AGENT_OS'] === "Linux") {
+    let version = tl.getInput('version');
+    let endpoint = tl.getInput('k8sService');
+    let kubeconfig = tl.getEndpointAuthorizationParameter(endpoint, 'kubeconfig', true);
+    let argLine = tl.getInput('cmd');
+    install.installOc(version)
+        .then(function (ocPath: string | null) {
+            if (ocPath === null) {
+                throw "No oc binary found"
+            }
+            return oc.execOc(kubeconfig, ocPath, argLine)
+        })
+        .then(function () {
+            tl.setResult(tl.TaskResult.Succeeded, "oc command successfully executed.");
+        })
+        .catch(function (err) {
+            tl.setResult(tl.TaskResult.Failed, err);
+            return;
+        })
+        ;
+} else {
+    tl.setResult(tl.TaskResult.Failed, "Task needs to run on an Linux agent.");
+}
 
-install.installOc(version)
-    .then(function (ocPath: string | null) {
-        if (ocPath === null) {
-            throw "No oc binary found"
-        }
-        return oc.execOc(kubeconfig, ocPath, argLine)
-    })
-    .then(function () {
-        tl.setResult(tl.TaskResult.Succeeded, "oc command successfully executed.");
-    })
-    .catch(function (err) {
-        tl.setResult(tl.TaskResult.Failed, err);
-        return;
-    })
-    ;
+
+
--- tasks/oc-cmd/src/tests/L0.ts
+++ tasks/oc-cmd/src/tests/L0.ts
@@ -1,7 +1,3 @@
-/// <reference path="../../node_modules/@types/chai/index.d.ts" />
-/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
-/// <reference path="../../node_modules/@types/node/index.d.ts" />
-
 let fs = require('fs-extra');
 let expect = require('chai').expect;
 let cmd = require("../oc-cmd");
@@ -17,12 +13,12 @@ describe('#setupConfig', function () {
 
     after(() => {
         fs.remove(`${__dirname}/../../out/.kube`)
-        .then(() => {
-          console.log('success!')
-        })
-        .catch((err: any) => {
-          console.error(err)
-        })
+            .then(() => {
+                console.log('success!')
+            })
+            .catch((err: any) => {
+                console.error(err)
+            })
         delete process.env['SYSTEM_DEFAULTWORKINGDIRECTORY']
         delete process.env['KUBECONFIG']
     });
@@ -31,33 +27,36 @@ describe('#setupConfig', function () {
         let testWorkingDir = `${__dirname}/../../out`
         process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] = testWorkingDir
         let config = "my dummy kube config"
-        expect(cmd.setupConfig(config)).to.be.undefined;
-        expect(fs.existsSync(`${testWorkingDir}/.kube/config`)).to.be.true;
-        let kubeconfig = process.env['KUBECONFIG']
-        if (kubeconfig === undefined) {
-            expect.fail("PATH not set")
-        } else {
-            expect(kubeconfig.includes(`${testWorkingDir}/.kube/config`)).to.be.true;
-        }        
+        cmd.setupConfig(config).then((result: undefined) => {
+            expect(result).to.be.undefined;
+            expect(fs.existsSync(`${testWorkingDir}/.kube/config`)).to.be.true;
+
+            let kubeconfig = process.env['KUBECONFIG']
+            if (kubeconfig === undefined) {
+                expect.fail("PATH not set")
+            } else {
+                expect(kubeconfig.includes(`${testWorkingDir}/.kube/config`)).to.be.true;
+            }
+        });
     });
 });
 
-describe('#tarballURL', function() {
-    it('should return null when the tag is empty', function() {
-        return install.tarballURL("").then( (result: string) => {
+describe('#tarballURL', function () {
+    it('should return null when the tag is empty', function () {
+        return install.tarballURL("").then((result: string) => {
             expect(result).to.be.null;
-          });
+        });
     });
 
-    it('should return null when the tag is null', function() {
-        return install.tarballURL(null).then( (result: any) => {
+    it('should return null when the tag is null', function () {
+        return install.tarballURL(null).then((result: any) => {
             expect(result).to.be.null;
-          });
+        });
     });
 
-    it('should return null when the tag is invalid', function() {
-        return install.tarballURL("foo").then( (result: any) => {
+    it('should return null when the tag is invalid', function () {
+        return install.tarballURL("foo").then((result: any) => {
             expect(result).to.be.null;
-          });
+        });
     });
 });
