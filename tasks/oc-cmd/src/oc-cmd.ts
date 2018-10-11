ce41b0647ac792672ef94e81618da000bf518a5a
Hardy Ferentschik <hardy@ferentschik.de>
2018-10-11 15:22:19 +0200

    Issue #15 Adding dependnecy to prettier and npm scripts for running it

--- tasks/oc-cmd/src/oc-cmd.ts
+++ tasks/oc-cmd/src/oc-cmd.ts
@@ -1,53 +1,57 @@
-"use strict"
+'use strict';
 
 import tl = require('vsts-task-lib/task');
 import { ToolRunner } from 'vsts-task-lib/toolrunner';
 
 const sub = require('substituter');
-const split = require('argv-split')
+const split = require('argv-split');
 
 /**
  * Prepares oc for execution and runs the specified command.
- * 
+ *
  * @param kubeConfig the kubeconfig for authentication to the cluster
  * @param ocPath absolute path to the oc binary
  * @param argLine the command to run
  */
-export async function execOc(kubeConfig: string, ocPath: string, argLine: string): Promise<void> {
-    await setupConfig(kubeConfig);
-   
-    let oc: ToolRunner = tl.tool(ocPath);
-    for (var arg of prepareOcArguments(argLine)) {
-        oc.arg(arg)
-    }
-    await oc.exec();
-    return;
+export async function execOc(
+  kubeConfig: string,
+  ocPath: string,
+  argLine: string
+): Promise<void> {
+  await setupConfig(kubeConfig);
+
+  let oc: ToolRunner = tl.tool(ocPath);
+  for (var arg of prepareOcArguments(argLine)) {
+    oc.arg(arg);
+  }
+  await oc.exec();
+  return;
 }
 
 /**
  * Splits the specified argument line into tokens and interpolates potential environment variables.
- * 
+ *
  * @param argLine The command line arguments as single string
  * @return array of arguments with potential environment variables interpolated
  */
 export function prepareOcArguments(argLine: string): string[] {
-    var interpolatedArgs = sub(argLine, process.env);
-    return split(interpolatedArgs);
+  var interpolatedArgs = sub(argLine, process.env);
+  return split(interpolatedArgs);
 }
 
 /**
  * Writes the cluster auth config to disk and sets the KUBECONFIG env variable
- * 
+ *
  * @param config The cluster auth config to write to disk
  */
 export async function setupConfig(config: string) {
-    let kubeConfigDir = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] + "/.kube";
-    if (!tl.exist(kubeConfigDir)) {
-        let mkdir: ToolRunner = tl.tool("mkdir");
-        mkdir.arg("-p").arg(kubeConfigDir);
-        await mkdir.exec();
-    }
-    let kubeConfig = kubeConfigDir + "/config"
-    tl.writeFile(kubeConfig, config);
-    tl.setVariable("KUBECONFIG", kubeConfig);
+  let kubeConfigDir = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] + '/.kube';
+  if (!tl.exist(kubeConfigDir)) {
+    let mkdir: ToolRunner = tl.tool('mkdir');
+    mkdir.arg('-p').arg(kubeConfigDir);
+    await mkdir.exec();
+  }
+  let kubeConfig = kubeConfigDir + '/config';
+  tl.writeFile(kubeConfig, config);
+  tl.setVariable('KUBECONFIG', kubeConfig);
 }
--- tasks/oc-cmd/src/tests/test-cmd-exec.ts
+++ tasks/oc-cmd/src/tests/test-cmd-exec.ts
@@ -2,47 +2,48 @@ import ma = require('vsts-task-lib/mock-answer');
 import tmrm = require('vsts-task-lib/mock-run');
 import path = require('path');
 
-let taskPath = path.join(__dirname, '..', '..', "lib", 'task.js');
+let taskPath = path.join(__dirname, '..', '..', 'lib', 'task.js');
 let tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
 process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] = '/tmp';
-process.env['ENDPOINT_AUTH_PARAMETER_K8SENDPOINT_KUBECONFIG'] = 'dummy kube config'
-process.env['AGENT_OS'] = 'Linux'
+process.env['ENDPOINT_AUTH_PARAMETER_K8SENDPOINT_KUBECONFIG'] =
+  'dummy kube config';
+process.env['AGENT_OS'] = 'Linux';
 
 tr.setInput('k8sService', 'k8sendpoint');
 tr.setInput('version', 'v3.9.0');
 tr.setInput('cmd', 'get nodes');
 
 let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
-    "exec": {
-        'mkdir -p /tmp/.download': {
-            "code": 0,
-            "stdout": ""
-        },
-        'curl -L -o /tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz': {
-            "code": 0,
-            "stdout": ""
-        },
-        'tar -xvf /tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz -C /tmp/.download': {
-            "code": 0,
-            "stdout": ""
-        },
-        'mkdir -p /tmp/.kube': {
-            "code": 0,
-            "stdout": ""
-        },
-        '/tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit/oc get nodes' : {
-            "code": 0,
-            "stdout": ""           
-        }
+  exec: {
+    'mkdir -p /tmp/.download': {
+      code: 0,
+      stdout: ''
     },
-    "exist": {
-        '/tmp/.download': true,
-        '/tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit': false,
-        '/tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit/oc': true,
-        '/tmp/.kube' : false
+    'curl -L -o /tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz': {
+      code: 0,
+      stdout: ''
+    },
+    'tar -xvf /tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz -C /tmp/.download': {
+      code: 0,
+      stdout: ''
+    },
+    'mkdir -p /tmp/.kube': {
+      code: 0,
+      stdout: ''
+    },
+    '/tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit/oc get nodes': {
+      code: 0,
+      stdout: ''
     }
-}
+  },
+  exist: {
+    '/tmp/.download': true,
+    '/tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit': false,
+    '/tmp/.download/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit/oc': true,
+    '/tmp/.kube': false
+  }
+};
 tr.setAnswers(a);
 
 tr.run();
