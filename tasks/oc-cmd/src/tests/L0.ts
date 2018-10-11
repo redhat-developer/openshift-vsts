5453d08ca4300c94a88e6e12d7f9c3580ce18844
Hardy Ferentschik <hardy@ferentschik.de>
2018-10-11 12:08:40 +0200

    Issue #21 Making sure environment variables get interpolated in oc command

--- tasks/oc-cmd/src/tests/L0.ts
+++ tasks/oc-cmd/src/tests/L0.ts
@@ -60,3 +60,26 @@ describe('#tarballURL', function () {
         });
     });
 });
+
+describe('#prepareOcArguments', function () {
+    before(() => {
+        delete process.env['FOO']
+        process.env['VSTS_TEST_VAR'] = "nodes"
+    });
+
+    after(() => {
+        delete process.env['VSTS_TEST_VAR']
+    });
+
+    it('should split arguments', function () {
+        expect(cmd.prepareOcArguments("get nodes")).to.be.an('array').that.include.ordered.members(['get', 'nodes']);
+    });
+
+    it('interpolate environment variables', function () {
+        expect(cmd.prepareOcArguments("get ${VSTS_TEST_VAR}")).to.be.an('array').that.include.ordered.members(['get', 'nodes']);
+    });
+
+    it('leave unkown environment variables intact', function () {
+        expect(cmd.prepareOcArguments("get ${FOO}")).to.be.an('array').that.include.ordered.members(['get', '${FOO}']);
+    });  
+});
