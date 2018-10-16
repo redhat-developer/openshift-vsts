e2d74855449891695aece661b3a5f746b99ac1ce
Hardy Ferentschik <hardy@ferentschik.de>
2018-10-16 13:54:23 +0200

    Issue #32 Adding Azure DevOps pipeline config for release
    - Using RedHatDevelopers and RedHatDevelopersStaging as publishers
    - Fixing tests to ony run L*.ts as tst files
    - Refactored variables in test
    - Refactored script tasks

--- tasks/oc-cmd/src/tests/test-agent-match.ts
+++ tasks/oc-cmd/src/tests/test-agent-match.ts
@@ -1,10 +1,16 @@
-import tmrm = require('vsts-task-lib/mock-run');
+import mockAnswer = require('vsts-task-lib/mock-answer');
+import mockRun = require('vsts-task-lib/mock-run');
 import path = require('path');
 
 let taskPath = path.join(__dirname, '..', '..', 'lib', 'task.js');
-let tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+let tr: mockRun.TaskMockRunner = new mockRun.TaskMockRunner(taskPath);
 
 process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] = '/opt/vsts/work/r1/a';
-process.env['AGENT_OS'] = 'Windows_NT';
 
+let a: mockAnswer.TaskLibAnswers = <mockAnswer.TaskLibAnswers>{
+  osType: {
+    osType: 'Windows'
+  }
+};
+tr.setAnswers(a);
 tr.run();
