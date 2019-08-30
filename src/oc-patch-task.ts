9c147aed7e25edf9b52952d8537fbcdd68201931
Luca Stocchi <lstocchi@redhat.com>
2019-08-30 13:46:22 +0200

    added new input json (#35)

---
+++ src/oc-patch-task.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import task = require('vsts-task-lib/task');
+import oc = require('./oc-exec');
+
+import * as install from './oc-install';
+import * as auth from './oc-auth';
+
+async function run() {
+  let version = task.getInput('version');
+  let argLine = task.getInput('cmd');
+  let jsonPatch = task.getInput('json');
+  let agentOS = task.osType();
+
+  let ocPath = await install.installOc(version, agentOS);
+  if (ocPath === null) {
+    throw new Error('no oc binary found');
+  }
+
+  await auth.createKubeConfig(auth.getOpenShiftEndpoint(), ocPath, agentOS);
+  argLine = `${argLine} '${jsonPatch}'`;
+  await oc.execOc(ocPath, argLine);
+}
+
+run()
+  .then(function() {
+    task.setResult(
+      task.TaskResult.Succeeded,
+      'oc command successfully executed.'
+    );
+  })
+  .catch(function(err: Error) {
+    task.setResult(task.TaskResult.Failed, err.message);
+  });
