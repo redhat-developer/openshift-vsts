10cd16ad3f469a37eadb1e65a44e2a8aaef66ccd
Luca Stocchi <lstocchi@redhat.com>
2019-09-05 16:33:01 +0200

    added new task to execute list of oc cmds (#92)

---
+++ src/oc-exec-list-tasks.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+import task = require('vsts-task-lib/task');
+import oc = require('./oc-exec');
+
+import * as install from './oc-install';
+import * as auth from './oc-auth';
+
+async function run() {
+  let version = task.getInput('version');
+  let argLine = task.getInput('cmds');
+  let agentOS = task.osType();
+
+  let ocPath = await install.installOc(version, agentOS);
+  if (ocPath === null) {
+    throw new Error('no oc binary found');
+  }
+
+  await auth.createKubeConfig(auth.getOpenShiftEndpoint(), ocPath, agentOS);
+  const cmds = argLine.split('\n');
+  task.debug(`cmds length ${cmds.length}`);
+  for (const cmd of cmds) {
+    await oc.execOc(ocPath, cmd);
+  }
+  
+}
+
+run()
+  .then(function() {
+    task.setResult(
+      task.TaskResult.Succeeded,
+      'oc command successfully executed.'
+    );
+  })
+  .catch(function(err: Error) {
+    task.setResult(task.TaskResult.Failed, err.message);
+  });
