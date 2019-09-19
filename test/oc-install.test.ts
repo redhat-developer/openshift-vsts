79b1f136dd44845837a58ec544dd468f3454e88a
Luca Stocchi <lstocchi@redhat.com>
2019-09-19 17:47:45 +0200

    removed all tests which downloaded files, fixed all not working tests (#75)

--- test/oc-install.test.ts
+++ test/oc-install.test.ts
@@ -5,23 +5,29 @@ import * as sinon from 'sinon';
 import * as fs from 'fs';
 
 import { InstallHandler } from '../src/oc-install';
-const validUrl = require('valid-url');
+import * as validUrl from 'valid-url';
+
+//const octokit = require('@octokit/rest')();
+//import * as octokit from '@octokit/rest';
+import tl = require('vsts-task-lib/task');
 
 describe('InstallHandler', function() {
 
+    let sandbox: sinon.SinonSandbox; 
+    const testOutDir = `${__dirname}/../out/test/ocInstall`;
+  
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+        process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] = testOutDir;
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+        delete process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'];
+    });
+
     describe('#ocInstall', function() {
-        let sandbox: sinon.SinonSandbox; 
-        const testOutDir = `${__dirname}/../out/test/ocInstall`;
-  
-        beforeEach(() => {
-            sandbox = sinon.createSandbox();
-            process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] = testOutDir;
-        });
-  
-        afterEach(() => {
-            sandbox.restore();
-            delete process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'];
-        });
 
         it('check if latestStable method is called if no ocVersion is passed', async function() {
             const latestStub = sandbox.stub(InstallHandler, 'latestStable').resolves('path');
@@ -43,7 +49,7 @@ describe('InstallHandler', function() {
 
         it('check if ocBundleUrl is correctly called if no valid url is passed', async function() {
             sandbox.stub(fs, 'existsSync').returns(true);
-            sandbox.stub(validUrl, 'isWebUri').returns(null);
+            sandbox.stub(validUrl, 'isWebUri').returns('');
             const ocBundleStub = sandbox.stub(InstallHandler, 'ocBundleURL').resolves(null);
             await InstallHandler.installOc('', 'Darwin');
             expect(ocBundleStub.calledOnce).to.be.true;
@@ -51,7 +57,7 @@ describe('InstallHandler', function() {
 
         it('check if method returns a null value if url is not valid', async function() {
             sandbox.stub(fs, 'existsSync').returns(true);
-            sandbox.stub(validUrl, 'isWebUri').returns(null);
+            sandbox.stub(validUrl, 'isWebUri').returns('');
             sandbox.stub(InstallHandler, 'ocBundleURL').resolves(null);
             const result = await InstallHandler.installOc('path', 'Darwin');
             expect(result).to.be.null;
@@ -81,6 +87,84 @@ describe('InstallHandler', function() {
             sandbox.stub(InstallHandler, 'downloadAndExtract').resolves('path');
             const result = await InstallHandler.installOc('path', 'Darwin');
             expect(result).equals('path');
+        });
+    });
+
+    describe('#ocBundleURL', function() {        
+
+        it('should return null when the tag is empty', async function() {
+            const result = await InstallHandler.ocBundleURL('', 'Linux');
+            expect(result).to.be.null;
+        });
+    
+        it('should return null when the tag is null', async function() {
+            const result = await InstallHandler.ocBundleURL(null, 'Linux');
+            expect(result).to.be.null;
+        });    
+       
+        // it('check if getReleaseByTag is called correctly', async function() {
+        //     // const args = {
+        //     //     owner: 'openshift',
+        //     //     repo: 'origin',
+        //     //     tag: 'foo'
+        //     // };
+            
+        //     //const Oktokit = new octokit();            
+        //     const oktoStub = sandbox.stub(octokit.repos, 'getReleaseByTag').rejects();
+        //     await InstallHandler.ocBundleURL('foo', 'Linux');
+        //     sandbox.assert.calledOnce(oktoStub);
+        // });
+    });
+
+    describe('#addOcToPath', function() {
+
+        it('adds oc to PATH under Windows', function() {
+          let ocDir =
+            'D:\\a\\r1\\a\\.download\\openshift-origin-client-tools-v3.10.0-dd10d17-windows';
+          expect(tl.getVariable('PATH')).to.not.contain(ocDir);
+          return InstallHandler.addOcToPath(`${ocDir}\\oc.exe`, 'Windows_NT').then(() => {
+            expect(tl.getVariable('PATH')).to.contain(ocDir);
+          });
+        });
+    
+        it('adds oc to PATH under macOS', function() {
+          let ocDir =
+            '/a/r1/a/.download/openshift-origin-client-tools-v3.10.0-dd10d17-mac';
+          expect(tl.getVariable('PATH')).to.not.contain(ocDir);
+          return InstallHandler.addOcToPath(`${ocDir}/oc`, 'Darwin').then(() => {
+            expect(tl.getVariable('PATH')).to.contain(ocDir);
+          });
+        });
+    
+        it('adds oc to PATH under Linux', function() {
+          let ocDir =
+            '/a/r1/a/.download/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit';
+          expect(tl.getVariable('PATH')).to.not.contain(ocDir);
+          return InstallHandler.addOcToPath(`${ocDir}/oc`, 'Linux').then(() => {
+            expect(tl.getVariable('PATH')).to.contain(ocDir);
+          });
+        });
+    
+        it('throws error with null path', function() {
+          return InstallHandler
+            .addOcToPath(null, 'Linux')
+            .then(() => {
+              expect.fail('call should not succeed');
+            })
+            .catch(function(err: Error) {
+              expect(err.message).to.eq('path cannot be null or empty');
+            });
+        });
+    
+        it('throws error with empty path', function() {
+          return InstallHandler
+            .addOcToPath('', 'Linux')
+            .then(() => {
+              expect.fail('call should not succeed');
+            })
+            .catch(function(err: Error) {
+              expect(err.message).to.eq('path cannot be null or empty');
+            });
         });
     });
 });
