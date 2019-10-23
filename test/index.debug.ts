44cd7122dc2bd396ea9c67ced512ac20a9c074fd
Luca Stocchi <lstocchi@redhat.com>
2019-10-23 14:58:44 +0200

    fixed indentation error - prettier check (#75)

--- test/index.debug.ts
+++ test/index.debug.ts
@@ -8,43 +8,43 @@ import * as Mocha from 'mocha';
 import * as path from 'path';
 
 process.on('unhandledRejection', err => {
-    console.log('Unhandled rejection:', err);
+  console.log('Unhandled rejection:', err);
 });
 
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
 export function run(): Promise<void> {
-    // Create the mocha test
-    const mocha = new Mocha({
-        ui: 'tdd',
-        useColors: true,
-        timeout: 50000,
-        slow: 50000
-    });
-    mocha.useColors(true);
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd',
+    useColors: true,
+    timeout: 50000,
+    slow: 50000
+  });
+  mocha.useColors(true);
 
-    const testsRoot = path.resolve(__dirname);
+  const testsRoot = path.resolve(__dirname);
 
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
+  return new Promise((c, e) => {
+    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return e(err);
+      }
 
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+      // Add files to the test suite
+      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-            try {
-                // Run the mocha test
-                mocha.run(failures => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    } else {
-                        c();
-                    }
-                });
-            } catch (err) {
-                e(err);
-            }
+      try {
+        // Run the mocha test
+        mocha.run(failures => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
         });
+      } catch (err) {
+        e(err);
+      }
     });
+  });
 }
