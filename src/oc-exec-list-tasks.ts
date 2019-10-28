b041b78b65693c559b91cce57799534e21857040
Luca Stocchi <lstocchi@redhat.com>
2019-10-28 12:53:32 +0100

    fixed error after rebased (#92)

--- src/oc-exec-list-tasks.ts
+++ src/oc-exec-list-tasks.ts
@@ -3,15 +3,15 @@
 import task = require('vsts-task-lib/task');
 import oc = require('./oc-exec');
 
-import * as install from './oc-install';
 import * as auth from './oc-auth';
+import { InstallHandler } from './oc-install';
 
 async function run() {
   let version = task.getInput('version');
   let argLine = task.getInput('cmds');
   let agentOS = task.osType();
 
-  let ocPath = await install.installOc(version, agentOS);
+  let ocPath = await InstallHandler.installOc(version, agentOS);
   if (ocPath === null) {
     throw new Error('no oc binary found');
   }
@@ -22,7 +22,6 @@ async function run() {
   for (const cmd of cmds) {
     await oc.execOc(ocPath, cmd);
   }
-  
 }
 
 run()
