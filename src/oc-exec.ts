3b84513f703a37c323c6bfc9d20b1e30f1ad17d6
Luca Stocchi <lstocchi@redhat.com>
2020-01-30 11:54:42 +0100

    refactor oc-exec (#130)

--- src/oc-exec.ts
+++ src/oc-exec.ts
@@ -44,7 +44,7 @@ export async function execOc(
   }
   // split cmd based on redirection operators
   const cmds = argLine.split(/(?=[>|\|])/);
-  const trs = prepareToolRunners(cmds, ocPath);
+  const trs = createToolRunners(cmds, ocPath);
   let i = 0;
   let trResult: ToolRunner = trs[i];  
   while (i + 1 < cmds.length) {
@@ -71,49 +71,56 @@ export async function execOc(
  * Splits the specified argument line into tokens and interpolates potential environment variables.
  *
  * @param argLine The command line arguments as single string
- * @param checkOc Flag to check if oc command is present in args
+ * @param removeOc Flag to check if oc command is present in args and remove it
  * @return array of arguments with potential environment variables interpolated
  */
-export function prepareOcArguments(argLine: string, checkOc?: boolean): string[] {
+export function prepareCmdArguments(argLine: string, removeOc?: boolean): string[] {
   let interpolatedArgs = sub(argLine, process.env);
   let args = split(interpolatedArgs);
-  if (checkOc && (args[0] === 'oc' || args[0] === 'oc.exe')) {
+  if (removeOc && (args[0] === 'oc' || args[0] === 'oc.exe')) {
     args = args.slice(1);
   }
   return args;
 }
 
-function prepareToolRunners(cmds: string[], ocPath: string): ToolRunner[] {
-  const trs: ToolRunner[] = [];
-  // first cmd in list is oc cmd for sure and user can omit "oc" command
-  let oc: ToolRunner = tl.tool(ocPath);
-  for (let arg of prepareOcArguments(cmds[0], true)) {
-    oc.arg(arg);
+function prepareToolRunner(cmd: string, ocPath: string): ToolRunner {
+  // first element in each command, without considering redirection operator, has to be the tool needed to execute it (e.g. oc, grep, findstr, ...)
+  let tr: ToolRunner;
+  if (cmd[0] === '>') {
+    return tr;
   }
-  trs.push(oc);
+
+  cmd = cmd[0] === '|' ? cmd.substring(1).trim() : cmd.trim();
+  const arg = prepareCmdArguments(cmd);
+  if (arg[0] === 'oc' || arg[0] === 'oc.exe') {
+    tr = tl.tool(ocPath);
+  } else {
+    tr = tl.tool(tl.which(arg[0], true));
+  } 
+  for (let argN of arg.slice(1)) {
+    tr.arg(argN);
+  }
+  return tr;
+}
+
+function createToolRunners(cmds: string[], ocPath: string): ToolRunner[] {
+  if (!cmds) {
+    return null;
+  }
+  const trs: ToolRunner[] = [];
+  // first cmd in list is oc cmd for sure and user can omit "oc"
+  if (cmds[0].length < 2 || cmds[0].substring(0, 2) !== 'oc') {
+    cmds[0] = `oc ${cmds[0]}`;
+  }
 
   // loop through concatenated commands
-  // user needs to explicitly write the tool needed (e.g. oc, grep, findstr, ...)
-  cmds = cmds.slice(1);
   for (let cmd of cmds) { 
-    if (cmd[0] === '>') {
+    const tr: ToolRunner = prepareToolRunner(cmd, ocPath);
+    if (!tr) {
       trs.push(null);
-      continue;
-    }
-
-    cmd = cmd[0] === '|' ? cmd.substring(1).trim() : cmd.trim();
-    const arg = prepareOcArguments(cmd);
-    let tr: ToolRunner;
-    if (arg[0] === 'oc' || arg[0] === 'oc.exe') {
-      tr = tl.tool(ocPath);
     } else {
-      tr = tl.tool(tl.which(arg[0], true));
-    } 
-    for (let argN of arg.slice(1)) {
-      tr.arg(argN);
+      trs.push(tr);
     }
-    
-    trs.push(tr);
   }
 
   return trs;
@@ -128,7 +135,7 @@ export function execOcSync(
   }
 
   let oc: ToolRunner = tl.tool(ocPath);
-  for (let arg of prepareOcArguments(argLine, true)) {
+  for (let arg of prepareCmdArguments(argLine, true)) {
     oc.arg(arg);
   }
 
