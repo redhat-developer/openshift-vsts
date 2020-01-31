2cef1a3581af7ebf789e29351bc702c350f66239
Luca Stocchi <lstocchi@redhat.com>
2020-01-31 17:28:12 +0100

    fixed failing tests (#130)

--- test/oc-run.test.ts
+++ test/oc-run.test.ts
@@ -13,31 +13,31 @@ describe('oc-run', function() {
     });
 
     it('should split arguments', function() {
-      expect(cmd.prepareOcArguments('get nodes'))
+      expect(cmd.prepareCmdArguments('get nodes'))
         .to.be.an('array')
         .that.include.ordered.members(['get', 'nodes']);
     });
 
     it('interpolate environment variables', function() {
-      expect(cmd.prepareOcArguments('get ${VSTS_TEST_VAR}'))
+      expect(cmd.prepareCmdArguments('get ${VSTS_TEST_VAR}'))
         .to.be.an('array')
         .that.include.ordered.members(['get', 'nodes']);
     });
 
     it('leave unknown environment variables intact', function() {
-      expect(cmd.prepareOcArguments('get ${FOO}'))
+      expect(cmd.prepareCmdArguments('get ${FOO}'))
         .to.be.an('array')
         .that.include.ordered.members(['get', '${FOO}']);
     });
 
     it('remove leading oc', function() {
-      expect(cmd.prepareOcArguments('oc get nodes', true))
+      expect(cmd.prepareCmdArguments('oc get nodes', true))
         .to.be.an('array')
         .that.include.ordered.members(['get', 'nodes']);
     });
 
     it('remove leading oc.exe', function() {
-      expect(cmd.prepareOcArguments('oc.exe get nodes', true))
+      expect(cmd.prepareCmdArguments('oc.exe get nodes', true))
         .to.be.an('array')
         .that.include.ordered.members(['get', 'nodes']);
     });
